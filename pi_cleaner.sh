dce9686efcf0067c7d4e6b9d3cc9103bc07a32fb
manuti <manuti@gmail.com>
2013-03-20 23:30:46 +0100

    Create pi_cleaner.sh
    bash scrip

---
+++ pi_cleaner.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
+YELLOW="\033[1;33m"
+RED="\033[0;31m"
+ENDCOLOR="\033[0m"
+
+if [ $USER != root ]; then
+  echo -e $RED"Error: must be root"
+  echo -e $YELLOW"Exiting..."$ENDCOLOR
+  exit 0
+fi
+
+echo -e $YELLOW"Cleaning apt cache..."$ENDCOLOR
+aptitude clean
+
+echo -e $YELLOW"Removing old config files..."$ENDCOLOR
+sudo aptitude purge $OLDCONF
+
+echo -e $YELLOW"Emptying every trashes..."$ENDCOLOR
+rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
+rm -rf /root/.local/share/Trash/*/** &> /dev/null
+
+echo -e $YELLOW"Script Finished!"$ENDCOLOR
